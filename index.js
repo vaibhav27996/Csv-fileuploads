d1c7f8141497aebe249b3e4e73efdca07113e390
vaibhav27996 <vmahajan27996@gmail.com>
2023-02-02 14:58:52 +0530

    last commit

---
+++ index.js
@@ -0,0 +1,62 @@
+const express=require('express');
+const app=express();
+const port=3001;
+const expressLayouts = require('express-ejs-layouts');
+const db = require('./config/mongoose');
+
+const session = require('express-session');
+
+const sassMiddleware = require('node-sass-middleware');
+const flash = require('connect-flash');
+const customMware = require('./config/middleware');
+
+const path=require('path');
+
+app.use(session({
+    secret:'flashblog',
+    saveUninitialized: true,
+    resave: true
+}));
+  
+
+app.use(sassMiddleware({
+    src: path.join(__dirname,'./assets/scss'),
+    dest: path.join(__dirname,'./assets/css'),
+    debug: true,
+    outputStyle: 'extended',
+    prefix: '/css'
+}));
+
+app.use(express.urlencoded({extended:false}));
+
+
+
+app.use(express.static('./assets'));
+
+// make the uploads path available to the browser
+app.use('/uploads', express.static(__dirname + '/uploads'));
+
+
+
+app.use(expressLayouts);
+// extract style and scripts from sub pages into the layout
+app.set('layout extractStyles', true);
+app.set('layout extractScripts', true);
+
+// set up the view engine
+app.set('view engine', 'ejs');
+app.set('views', './views');
+
+app.use(flash());
+app.use(customMware.setFlash);
+
+// use express router
+app.use('/', require('./routes'));
+
+app.listen(port, function(err){
+    if (err){
+        console.log(`Error in running the server: ${err}`);
+    }
+
+    console.log(`Server is running on port: ${port}`) ;
+});
